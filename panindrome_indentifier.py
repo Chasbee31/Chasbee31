9179abd9b3089a0fb3bb09670ea5ea82b3d22389
Chasbee31 <79656885+Chasbee31@users.noreply.github.com>
2022-03-13 18:55:00 +0000

    Comparison for loop efficiency update
    Added if statement within for loop which ensure only the first half of the string is checked.
    Second half is redundant if first half passes.

--- panindrome_indentifier.py
+++ panindrome_indentifier.py
@@ -24,7 +24,9 @@ def main():
 
         #For loop  compares each character 1 by 1, if it fails at any point it exits the loop immediately
         for n,c in enumerate(palindrome):
-            if c != reversed[n]:
+            if n == int(len(palindrome)/2): #Added as it only needs to ensure the first half of the string doesn't fail
+                break
+            elif c != reversed[n]:
                 result = "Palindrome test: False"
                 break
             else:
